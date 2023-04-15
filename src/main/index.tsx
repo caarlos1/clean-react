cc7b9fb5ee870155cc28508b8cfc2084b381aaac
Carlos Roberto <carlos.junior@dotgroup.com.br>
2023-04-15 17:00:36 -0300

    feat: add strict mode to app

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 
 import Login from '@/presentation/pages/login/login'
 
-ReactDOM.render(<Login />, document.getElementById('main'))
+ReactDOM.render(
+  <StrictMode>
+    <Login />
+  </StrictMode>,
+  document.getElementById('main')
+)
