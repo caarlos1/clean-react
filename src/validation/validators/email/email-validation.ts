76b43d0db49d554f7c87d7a18429f1976f730e20
Carlos Roberto <crpu4a@gmail.com>
2023-05-03 21:48:06 -0300

    feat: ensure required email validation returns falsy if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -4,6 +4,8 @@ import type { FieldValidation } from '@/validation/protocols'
 export class EmailValidation implements FieldValidation {
   constructor(readonly field: string) {}
   validate(value: string): InvalidFieldError {
-    return new InvalidFieldError()
+    const emailRegex =
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    return value.match(emailRegex) ? null : new InvalidFieldError()
   }
 }
