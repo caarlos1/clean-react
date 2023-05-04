a407bd258020fcd77efd8e2074cc2ca9e3697b48
Carlos Roberto <crpu4a@gmail.com>
2023-05-03 23:05:07 -0300

    test: ensure min length validation returns error if value is invalid

---
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -0,0 +1,13 @@
+import { faker } from '@faker-js/faker'
+import { InvalidFieldError } from '@/validation/errors'
+import { MinLengthValidation } from './min-length-validation'
+
+const makeSut = (): MinLengthValidation => new MinLengthValidation(faker.database.column(), 5)
+
+describe('MinLengthValidation', () => {
+  test('Should return error if value is invalid', () => {
+    const sut = makeSut()
+    const error = sut.validate('123')
+    expect(error).toEqual(new InvalidFieldError())
+  })
+})
